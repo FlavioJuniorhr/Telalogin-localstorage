54ae0b2d2fa4a39a8d8d27a03449a6055d8685db
Juniorx <111861420+FlavioJuniorhr@users.noreply.github.com>
2023-05-19 08:23:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,76 @@
-window.addEventListener("load", function(){
-    let salvar = document.getElementById("btnAdd")
-    let Em = document.getElementById("exampleInputNome")
-    let Sn = document.getElementById("exampleInputnomem")
-    let conteudo = document.getElementById("conteudo")
-    let limpar = document.getElementById("btnLimp")
-  
-    salvar.addEventListener("click", function(){
-        let input1=Em.value
-        let input2=Sn.value
-        localStorage.setItem(input1, input2)
-  
-        exibir()
-    })
-    limpar.addEventListener("click", function(){
-        localStorage.clear()
+var Csn = document.getElementById("Senha")
+var Eem = document.getElementById("EEmail")
+var Esn = document.getElementById("ESenha")
+window.addEventListener("load", function () {
 
-        exibir()
-    })
+    //dashboard.textContent = exibir()
 
-    function exibir() {
-        let str = ""
-        for (let i = 0, len = localStorage.length; i < len; i++) {
-            let exampleInputNome = localStorage.key(i)
-            let Sn = localStorage.getItem(exampleInputNome)
-            str += `${[i+1]} . ${exampleInputNome} : ${Sn}  <br>`
+    var str = ""
+    for (let i = 0, len = localStorage.length; i < len; i++) {
+        let oi = localStorage.key(i)
+        let S = localStorage.getItem(oi)
+        ///str += `${[i + 1]}. Cadastro de: ${oi}<br>ㅤㅤEmail: ${S}  <br>`
+        /* let use = localStorage.getItem(eemail)
+        if (use) {
+            console('ok')
+            dashboard.innerHTML += `
+            <div id="userName">${[i + 1]}. Cadastro de: ${oi}</div>
+            <div id="userEmail">Email: ${S}</div>
+            `
         }
-
-        Sn.value = ""
-        Em.value = ""
-        conteudo.innerHTML = str
+        const nameDash = document.getElementById('userName')
+        const emailDash = document.getElementById('userEmail')
+        nameDash.innerHTML = `${[i + 1]}. Cadastro de: ${oi}`
+        emailDash.innerHTML = `Email: ${S}` */
     }
-    exibir()
 })
 
-//Cadastro
-function retornaV(conteudo) {
-    if (!("erro" in conteudo)) {
-        document.getElementById('endereco').value = (conteudo.logradouro)
-        document.getElementById('bairro').value = (conteudo.bairro)
-        document.getElementById('cidade').value = (conteudo.localidade)
-        document.getElementById('complemento').value = (conteudo.complemento)
-    } else {
-        alert("CEP não encontrado")
-    }
-}
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-function buscacep(valor) {
-    var cep = valor.replace(/\D/g, '')
-    if (cep != "") {
-        var validarcep = /^[0-9]{8}$/
-        if (validarcep.test(cep)) {
-            document.getElementById('endereco').value = ". . ."
-            document.getElementById('bairro').value = ". . ."
-            document.getElementById('cidade').value = ". . ."
-            document.getElementById('complemento').value = ". . ."
-            var script = document.createElement('script')
-            script.src = `https://viacep.com.br/ws/${cep}/json/?callback=retornaV`
-            document.body.appendChild(script)
-        } else {
-            alert("Formato inválido")
-        }
+    const usuario = document.getElementById('eemail').value;
+    const senha = document.getElementById('ssenha').value;
+    const cadastro = JSON.parse(localStorage.getItem(usuario));
+    function real() {
+        
     }
-}
+    var Ds = 0
+    var Dc = 0
+    var De = 0
+    for (let i = 0, len = localStorage.length; i < len; i++) {
+        let oi = localStorage.key(i)
+        let S = localStorage.getItem(oi)
+
+        if (!usuario || !senha) {
+            //  return showToast('error', 'Campos incompletos');
+            Ds = Ds + 1
+        
+
+            // return showToast('error', 'Usuário ou senha incorreta');
+        } else if (oi == senha && S == usuario) {
+            
+            // const dashboard = document.getElementById('dadosContainer')
+            // var str = ""
+            Dc = Dc + 1
+            ///str += `${[i + 1]}. Cadastro de: ${oi}<br>ㅤㅤEmail: ${S}  <br>`
+            /* dashboard.innerHTML += `
+                <div id="userName">${[i + 1]}. Cadastro de: ${oi}</div>
+                <div id="userEmail">Email: ${S}</div>
+                ` */
+        }else{
+            De = De + 1 
+        }
+        /* const nameDash = document.getElementById('userName')
+        const emailDash = document.getElementById('userEmail')
+        nameDash.innerHTML = `${[i + 1]}. Cadastro de: ${oi}`
+        emailDash.innerHTML = `Email: ${S}` */
+    }
+    if(Ds > 0){
+        alert("Sem dados")
+    }else if(Dc > 0){
+        alert("Seja bem vindo")
+    }else{
+        alert("Dados errados ou inclompletos")
+    }
+});
